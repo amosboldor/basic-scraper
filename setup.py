c7fd6615abbccba62710fe457e59441577a37321
Amos Boldor <amosboldor@gmail.com>
2017-01-15 12:44:01 -0800

    add setup.py for quick install

---
+++ setup.py
@@ -0,0 +1,20 @@
+"""Setup file for a quick install."""
+
+
+from setuptools import setup
+
+setup(
+    name="basic-scraper",
+    description="Basic scraper for Health Inspection Data from King County.",
+    version=0.1,
+    author=["Amos Boldor"],
+    author_email=["amosboldor@gmail.com"],
+    license="MIT",
+    package_dir={'': '.'},
+    py_modules=[
+        "beautifulsoup4"
+    ],
+    extras_require={
+        "test": ["pytest", "pytest-cov", "tox"]
+    }
+)
